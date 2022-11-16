9a70ee9fd349445464415654a71158c94683080c
Mateus Nec <mateus.veloso@necbrasil.com.br>
2022-11-16 20:28:26 -0300

    Primeiro commit

---
+++ dist/src/server.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./config/moduleAlias");
+//# sourceMappingURL=server.js.map
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import "./config/moduleAlias";
+import express from 'express';
+const app = express();
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log("Servidor Rodando");
+})
